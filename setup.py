6d0cfdc1f75c8b8e6b9ef5e2c06893ce07b7f9d2
John J. Workman <johnjworkman@gmail.com>
2011-07-28 17:21:48 -0400

    Adding setup.py file

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="Slushbox",
+    version="0.1",
+    author="John J. Workman",
+    author_email="workman@alumni.duke.edu",
+    url="https://github.com/workmajj/slushbox",
+    description="Reloads local web pages when files in their directories \
+        change.",
+    license = "BSD 3-Clause <http://www.opensource.org/licenses/BSD-3-Clause>",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: System :: Filesystems',
+        'Topic :: Utilities'
+    ],
+    install_requires=[
+        'MacFSEvents>=0.2.4'
+    ],
+    packages=['slushbox']
+)
